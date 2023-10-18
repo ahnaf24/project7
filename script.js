2c027dfa1b73b15d130ca2bc9ddaa3ff7fd67555
Test User <ahnaf.shakil24@gmail.com>
2023-10-18 13:37:59 +0600

    a

---
+++ script.js
@@ -0,0 +1,75 @@
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', e => { 
+               if ((e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight')
+                     && this.keys.indexOf(e.key) === -1){
+                this.keys.push(e.key);
+               }
+            });
+            window.addEventListener('keyUp', e => { 
+                if (e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight' ){
+                 this.keys.splice(this.keys.indexOf(e.key),1);
+                }
+             });
+        }
+    }
+
+    class Player {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = this.gameWidth;
+            this.gameHeight = this.gameHeight;
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = this.gameHeight - this.height;
+        }
+        draw(context){
+            context.fillstyle = 'white'; 
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+        update(){
+            this.x++;
+        }
+
+    }
+
+    class Background {
+
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+
+    }
+
+    function displayStatusText(){
+
+    }
+
+    const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+    player.draw(ctx);
+    player.update();
+
+    function animate(){
+        player.update();
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
